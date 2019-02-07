ebb7b6c215b130421b242d53ae6db5f2e7ce5e12
rochismo <alexis.platino@gmail.com>
2019-02-07 22:34:58 +0100

    Added basic websocket functionality

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const loader = require("./service/loader.js");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
 
 const port = 3000;
-
+const connections = [];
 // Sets
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -21,6 +23,11 @@ app.get("/", (req, res) => {
     res.render("index", {
         cases: loader.getCases()
     });
+})
+
+app.get("/get-posts", function(req, res) {
+    const data = require("./cases.json");
+    res.json(data);
 })
 
 app.post("/add", (req, res) => {
@@ -50,9 +57,24 @@ app.post("/add", (req, res) => {
         notes,
         lastId
     });
+    res.redirect("/");
     res.render("index", {cases: loader.getCases()})
 })
 
-app.listen(port, () => {
+io.sockets.on("connection", function(sock) {
+    connections.push(sock);
+    console.log(`${connections.length} sockets on`);
+
+    sock.on("disconnect", function(data) {
+        connections.splice(connections.indexOf(sock), 1);
+        console.log(`Disconnected: ${connections.length} sockets on`);
+    });
+
+    sock.on("post", function() {
+        io.sockets.emit("render", {log: "Added new post"});
+    })
+})
+
+server.listen(port, () => {
     console.log(`Listening @ http://localhost:${port}`);
 })
